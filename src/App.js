d3800200ca115d7a1fdcdef6f574043feedaec23
franklynobleC <franklyessien01@gmail.com>
2023-07-21 21:43:51 +0100

    reviews page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Review from './Review'
+
+import { FaGithubSquare } from 'react-icons/fa'
+function App () {
+  return (
+    <div>
+      <FaGithubSquare className='icon ' />
+      <Review />
+    </div>
+  )
+}
+
+export default App
